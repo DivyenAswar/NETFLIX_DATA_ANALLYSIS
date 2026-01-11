80a45e13124cec6dd047dcddfa9016d4d272aade
DivyenAswar <divyenaswar19@gmail.com>
2026-01-11 16:50:28 +0530

    Update Business Problem Netflix.sql

--- netflix_data_analysis/Business Problem Netflix.sql
+++ netflix_data_analysis/Business Problem Netflix.sql
@@ -1,35 +1,24 @@
-17 Business Problems & Solutions
+## Business Problems & Analysis Questions
 
-Count the total number of content items on Netflix
+1. Count the total number of content items on Netflix  
+2. Identify the different types of content available on Netflix  
+3. Count the number of unique directors on Netflix  
+4. Count the number of unique countries with Netflix content  
 
-Identify the different types of content available on Netflix
+5. Count the number of Movies vs TV Shows  
+6. List all movies released in a specific year (e.g., 2020)  
+7. Identify the longest movie available on Netflix  
+8. Find content added to Netflix in the last 5 years  
 
-Count the number of unique directors on Netflix
+9. Find all content directed by S. Shankar  
+10. List all TV Shows with more than 5 seasons  
+11. Count the number of content items in each genre  
 
-Count the number of unique countries with Netflix content
+12. Identify the top 5 years with the highest number of content releases in India  
+13. List all documentary movies available on Netflix  
+14. Find all movies that do not have a director listed  
 
-Count the number of Movies vs TV Shows
+15. Find all movies featuring Salman Khan released in the last 10 years  
+16. Identify the top 10 actors appearing in the highest number of Indian movies  
 
-List all movies released in a specific year (e.g., 2020)
-
-Identify the longest movie available on Netflix
-
-Find content added to Netflix in the last 5 years
-
-Find all content directed by S. Shankar
-
-List all TV Shows with more than 5 seasons
-
-Count the number of content items in each genre
-
-Identify the top 5 years with the highest number of content releases in India
-
-List all documentary movies available on Netflix
-
-Find all movies that do not have a director listed
-
-Find all movies featuring Salman Khan released in the last 10 years
-
-Identify the top 10 actors appearing in the highest number of Indian movies
-
-Categorize content as “Good” or “Bad” based on violence-related keywords in descriptions
+17. Categorize content as “Good” or “Bad” based on violence-related keywords in descriptions  
